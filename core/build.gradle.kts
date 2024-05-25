b3e150b0ed1fa705bb436c3d1b09b805180da844
ahmedgomaa <ahmedtolba@blackstoneeit.com>
2024-05-25 23:55:42 +0300

    add core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    kotlin("kapt")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.gomaa.core"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    android.buildFeatures.buildConfig = true
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            buildConfigField("String", "BASE_URL", "\"https://api.nytimes.com/\"")
+        }
+        release {
+            isMinifyEnabled = false
+            buildConfigField("String", "BASE_URL", "\"https://api.nytimes.com/\"")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    // Networking
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+
+    // Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+
+    implementation("com.google.code.gson:gson:2.11.0")
+
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    implementation("com.google.dagger:hilt-android:2.49")
+    implementation("androidx.compose.ui:ui-test-junit4-android:1.6.7")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    kapt("com.google.dagger:hilt-compiler:2.48.1")
+    implementation( "androidx.hilt:hilt-navigation-compose:1.2.0")
+}
